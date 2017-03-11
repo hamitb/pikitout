9c26b56a1136116b42f96a18a1d31e21c841bf12
hamitburakemre <hamitburakemre@gmail.com>
2017-03-11 22:59:34 +0300

    Completed updating posts with Firebase data

--- SocialApp/FeedVC.swift
+++ SocialApp/FeedVC.swift
@@ -7,30 +7,48 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
 
-        DataService.ds.REF_POSTS.observe(.value) { (snapshot) in
-            print(snapshot.value)
-        }
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("HMTBRK: \(post.caption)")
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        
+        return posts.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
---
+++ SocialApp/Post.swift
@@ -0,0 +1,81 @@
+//
+//  Post.swift
+//  SocialApp
+//
+//  Created by Hamit Burak Emre on 11/03/17.
+//  Copyright © 2017 Hamit Burak Emre. All rights reserved.
+//
+
+import Foundation
+
+
+class Post {
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+        return _caption
+    }
+
+    var imageUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._likes = likes
+        self._imageUrl = imageUrl
+    }
+    
+    init(postKey: String, postData: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let caption = postData["caption"] as? String {
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
